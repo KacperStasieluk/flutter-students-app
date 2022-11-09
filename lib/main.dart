bf075e9cc286d3c4031f97660375f04353cac96f
D1g1L34d3r <61713593+KacperStasieluk@users.noreply.github.com>
2022-11-09 11:11:16 +0100

    x

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:convert' show utf8;
+import 'dart:typed_data';
+//import 'dart:html' as html;
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -17,13 +21,18 @@ class HomePage extends StatefulWidget {
   HomePageState createState() => HomePageState();
 }
 
+
 class HomePageState extends State<HomePage> {
 
   Map<String, dynamic> data = {};
+  int dataLength = 0;
   bool _customTileExpanded = false;
-  File file = File('/cache/cache.txt');
+
+
+
 
   Future<String> getData() async {
+
     var response = await http.get(
         Uri.parse("https://raw.githubusercontent.com/KacperStasieluk/flutter-students-app/main/students.json"),
         headers: {
@@ -33,27 +42,22 @@ class HomePageState extends State<HomePage> {
 
     setState(() {
       print(response.statusCode);
-      if (response.statusCode == 200)
-        {
-          data = jsonDecode(response.body);
-        }
-      else
-        {
-          file.readAsString().then((String content) =>
-          {
-            data = jsonDecode(content)
-          });
-        }
+      data = jsonDecode(response.body);
+      dataLength = data["students"].length;
     });
-
-    file = await file.writeAsString(data.toString());
 
     return "Success!";
   }
 
+  Future<String> networkImageToBase64(String imageUrl) async {
+    http.Response response = await http.get(Uri.parse(imageUrl));
+    final bytes = response.bodyBytes;
+    return base64Encode(bytes).toString();
+  }
+
   @override
-  void initState(){
-    this.getData();
+  void initState() {
+    getData();
   }
 
   @override
@@ -62,16 +66,30 @@ class HomePageState extends State<HomePage> {
   const title = "Studenci";
 
     List<Widget> mywidgets = [];
-    for(var x = 0; x < data["students"].length; x++){
+    for(var x = 0; x < dataLength; x++){
+
+
+      print("ello");
+      //html.window.localStorage["user"+x.toString()] = networkImageToBase64(data["students"][x]["img"]) as String;//Image(image: NetworkImage(data["students"][x]["img"])).toBa;
+      //print(html.window.localStorage["user1"]);
+      print("naura");
 
       List<Widget> tiles = [];
       tiles.add(Align(
         alignment: Alignment.topLeft,
+          //child: Image(image: base64Decode(html.window.localStorage["user1"] as String) as ImageProvider),
           child: CachedNetworkImage(
           placeholder: (context, url) => const CircularProgressIndicator(),
-          imageUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
+          imageUrl: data["students"][x]["img"],
           width: 50,
           height: 50,
+          cacheManager: CacheManager(
+              Config(
+                "fluttercampus",
+                stalePeriod: const Duration(seconds: 5),
+                //one week cache period
+              )
+          ),
     ),));
       for(var y = 0; y < data["students"][x]["grades"].length; y++){
           tiles.add(ListTile(
